f0cb989219da3263a3425c67eb59ad0d0ad911ef
pnapoli <napoli.p@gmail.com>
2016-03-27 20:38:13 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -29,50 +29,60 @@ x_data <- rbind(x_train, x_test)
 y_data <- rbind(y_train, y_test)
 
 
+# -----------------------------------------------------------------
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# -----------------------------------------------------------------
+f <- read.table("features.txt")
 
-# Step 2
-# Extract only the measurements on the mean and standard deviation for each measurement
-###############################################################################
+#
+# extracting only columns with mean() or std()
+#
+msf <- grep("-(mean|std)\\(\\)", f[, 2])
 
-features <- read.table("features.txt")
+#
+# subset the selected columns
+#
+x_data <- x_data[, msf]
 
-# get only columns with mean() or std() in their names
-mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+#
+# correcting the column names
+#
+names(x_data) <- f[msf, 2]
 
-# subset the desired columns
-x_data <- x_data[, mean_and_std_features]
+# -----------------------------------------------------------------
+# 3. Uses descriptive activity names to name the activities in the data set
+# -----------------------------------------------------------------
 
-# correct the column names
-names(x_data) <- features[mean_and_std_features, 2]
+act <- read.table("activity_labels.txt")
 
-# Step 3
-# Use descriptive activity names to name the activities in the data set
-###############################################################################
+#
+# updating values using the correct activity names
+#
+y_data[, 1] <- act[y_data[, 1], 2]
 
-activities <- read.table("activity_labels.txt")
-
-# update values with correct activity names
-y_data[, 1] <- activities[y_data[, 1], 2]
-
-# correct column name
+#
+# correcting the column name
+#
 names(y_data) <- "activity"
 
-# Step 4
-# Appropriately label the data set with descriptive variable names
-###############################################################################
+# -----------------------------------------------------------------
+# 4. Appropriately labels the data set with descriptive variable names.
+# -----------------------------------------------------------------
 
-# correct column name
+#
+# correcting column name
+#
 names(subject_data) <- "subject"
 
-# bind all the data in a single data set
+#
+# merging all the data in one data set
+#
 all_data <- cbind(x_data, y_data, subject_data)
 
-# Step 5
-# Create a second, independent tidy data set with the average of each variable
-# for each activity and each subject
-###############################################################################
+# -----------------------------------------------------------------
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# -----------------------------------------------------------------
 
-# 66 <- 68 columns but last two (activity & subject)
-averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+avg_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
-write.table(averages_data, "averages_data.txt", row.name=FALSE)
+write.table(avg_data, "averages_data.txt", row.name=FALSE)
